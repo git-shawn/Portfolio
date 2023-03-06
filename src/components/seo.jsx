ee3a82c5be8261fc482b9135435460ca47f3bb19
Glitch (glitch-hello-react) <none>
2023-03-06 22:19:46 +0000

    💰🚤 Checkpoint ./package.json:10820362/51 ./src/app.jsx:10820362/1527 ./src/pages/home.jsx:10820362/4596 ./src/components/router.jsx:10820362/275 ./src/styles/styles.css:10820362/793 ./src/components/seo.jsx:10820362/278 ./src/seo.json:10820362/172 ./src/pages/about.jsx:10820362/387 ./src/pages/apps.jsx:10820362/9

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -3,12 +3,8 @@ import SEO from "../seo.json";
 import { Helmet } from 'react-helmet-async';
 
 const Seo = () => {
-  // If url is set to 'glitch-default', we use the hostname for the current page
-  // Otherwise we use the value set in seo.json
   const url = SEO.url === 'glitch-default' ? window.location.hostname : SEO.url
   
-  // React Helmet manages the content of the page head such as meta tags
-  // We use the async package https://github.com/staylor/react-helmet-async
   return <Helmet>
     <title>{SEO.title}</title>
 
