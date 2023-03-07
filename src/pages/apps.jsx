007bb9d91a18f355900d9724c7abfe9636971f39
Glitch (glitch-hello-react) <none>
2023-03-07 20:45:29 +0000

    🏣🛋 Checkpoint ./src/pages/apps.jsx:10820362/3992 ./src/styles/styles.css:10820362/21819 ./.glitch-assets:10820362/384

--- src/pages/apps.jsx
+++ src/pages/apps.jsx
@@ -3,15 +3,24 @@ import * as React from "react";
 export default function Apps() {
   return (
     <div className="thinContainer">
-      <h1>
-        My Apps
-      </h1>
+      <h1>My Apps</h1>
       <p>
         Welcome to the Glitch React starter, where you can instantly create a
         React site that's fully customizable.
       </p>
       <div className="appGrid">
-      <p>Hello!</p>
+        <div className="appInfo">
+          <div className="appIcon">
+            <img
+              src="https://cdn.glitch.global/de491910-0060-4fe1-87cc-7ff32c7bf88e/qrPopApp.png?v=1678221258807"
+              alt="QR Pop App Icon"
+            />
+          </div>
+          <div className="appMeta">
+            <h2>QR Pop</h2>
+            <p>Available for macOS and iOS</p>
+          </div>
+        </div>
       </div>
     </div>
   );
