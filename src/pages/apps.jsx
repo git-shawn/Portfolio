d833907f09e71d900b7bd754621fd630cde8fba5
Glitch (glitch-hello-react) <none>
2023-03-07 20:56:48 +0000

    📊🐥 Checkpoint ./src/styles/styles.css:10820362/236 ./src/pages/apps.jsx:10820362/2966 ./.glitch-assets:10820362/396

--- src/pages/apps.jsx
+++ src/pages/apps.jsx
@@ -1,26 +1,48 @@
 import * as React from "react";
+import { Router, Link, useRoute } from "wouter";
 
 export default function Apps() {
   return (
     <div className="thinContainer">
       <h1>My Apps</h1>
       <p>
-        Welcome to the Glitch React starter, where you can instantly create a
-        React site that's fully customizable.
+        Write about my motivations to create apps.
       </p>
       <div className="appGrid">
-        <div className="appInfo">
-          <div className="appIcon">
-            <img
-              src="https://cdn.glitch.global/de491910-0060-4fe1-87cc-7ff32c7bf88e/qrPopApp.png?v=1678221258807"
-              alt="QR Pop App Icon"
-            />
+        
+        {/* QR Pop */}
+        
+        <Link href="/">
+          <div className="appInfo">
+            <div className="appIcon">
+              <img
+                src="https://cdn.glitch.global/de491910-0060-4fe1-87cc-7ff32c7bf88e/qrPopApp.png?v=1678221258807"
+                alt="QR Pop App Icon"
+              />
+            </div>
+            <div className="appMeta">
+              <h2>QR Pop</h2>
+              <p>Available for macOS and iOS</p>
+            </div>
           </div>
-          <div className="appMeta">
-            <h2>QR Pop</h2>
-            <p>Available for macOS and iOS</p>
+        </Link>
+        
+        {/* Rerouter */}
+        
+        <Link href="/">
+          <div className="appInfo">
+            <div className="appIcon">
+              <img
+                src="https://cdn.glitch.global/de491910-0060-4fe1-87cc-7ff32c7bf88e/rerouterApp.webp?v=1678222179149"
+                alt="Rerouter Icon"
+              />
+            </div>
+            <div className="appMeta">
+              <h2>Rerouter</h2>
+              <p>Available for macOS and iOS</p>
+            </div>
           </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
